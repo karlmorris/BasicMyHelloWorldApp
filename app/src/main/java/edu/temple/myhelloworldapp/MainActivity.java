5f2e0a6a09f971e2bf5fec4d7b8f1da25ab0f447
karl <karl@macc.io>
2020-09-03 15:18:43 -0400

    Added a toast message

--- app/src/main/java/edu/temple/myhelloworldapp/MainActivity.java
+++ app/src/main/java/edu/temple/myhelloworldapp/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int a = 4;
-        int b = 235;
-        int total = a + b;
 
+        findViewById(R.id.clickMeButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 2;
+                int b = 45;
+
+                float answer = (float) b / a;
+                Toast.makeText(MainActivity.this, "The answer is " + answer, Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
 }
